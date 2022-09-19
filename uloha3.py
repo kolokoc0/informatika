b94516d7169d2f031bdc8c036d1cde756a87e624
kolokoc0 <kristiansvrlo@gmail.com>
2022-09-19 18:42:43 +0200

    uloha dudik

---
+++ uloha3.py
@@ -0,0 +1,5 @@
+a = int(input("Vloz cislo ktoreho hodnota bude prevratena: "))
+
+b = 1/a
+
+print("Obratena hodnota cisla "+str(a)+" Je: "+str(b))
---
+++ uloha6.py
@@ -0,0 +1,5 @@
+a = int(input("Is this number even ?: "))
+if a%2==0:
+    print ("Number "+str(a)+" is even")
+else:
+    print ("Number "+str(a)+" is odd")
