6b112f6765c4d81be2b32f28d094520cb0db94ff
kolokoc0 <kristiansvrlo@gmail.com>
2022-09-27 12:07:04 +0200

    removal

--- Ulohyforwhile/ulohafor1.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Konecna hranica uzavreteho intervalu"))
-
-for i in range (1,a+1):
-    if i%3 == 0:
-        print (i)
--- uloha6.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Is this number even ?: "))
-if a%2==0:
-    print ("Number "+str(a)+" is even")
-else:
-    print ("Number "+str(a)+" is odd")
