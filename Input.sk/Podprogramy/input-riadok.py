6a1162a9910c9e0a333692fea139f17192dfcb4f
kolokoc0 <113050432+kolokoc0@users.noreply.github.com>
2022-10-19 11:34:45 +0200

    Update input-riadok.py

--- Input.sk/Podprogramy/input-riadok.py
+++ Input.sk/Podprogramy/input-riadok.py
@@ -1,12 +1,27 @@
-def riadok(n, text):
+def riadok(n,text):
     vys = ""
     dlzte = len(text)
     if text!="":
+      if dlzte%2==0:
+            n -= (dlzte+2)
+            vys += (((n)//2)*"*")
+            vys += " "+ text + " "
+            vys += (((n)//2)*"*")
+      else:
         n -= (dlzte+2)
         vys += (((n)//2)*"*")
         vys += " "+ text + " "
-        vys += (((n)//2)*"*")
+        vys += ((((n)//2)+1)*"*")
     else:
         vys += n*"*"
     print (vys)
-riadok (int(input("Vloz pocet: ")),input("Vloz text: "))
+sir = 40
+riadok(sir, "")
+riadok(sir, 'Ján Botto')
+riadok(sir, 'Žltá ľalija')
+riadok(sir, '-')
+riadok(sir, 'Stojí stojí mohyla')
+riadok(sir, 'Na mohyle zlá chvíľa')
+riadok(sir, 'na mohyle tŕnie chrastie')
+riadok(sir, 'a v tom tŕní chrastí rastie')
+riadok(sir, "")
