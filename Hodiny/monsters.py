af9d437e078a454f872175d51ad7c91d8717e039
kolokoc0 <113050432+kolokoc0@users.noreply.github.com>
2023-02-07 15:49:12 +0100

    update monsters.py
    sex je tam

--- Hodiny/monsters.py
+++ Hodiny/monsters.py
@@ -9,10 +9,20 @@ def create_monster()->list:
 def iq_test(monster)->int:
     return monster.count(1)
 def sex(monster1,monster2):
+    babo = []
     #7 percentna mutacia
     #mutacia ak nastane ta 7% sanca tak vygenerujem na to miesto random cislo od 0-9
-    mutationindex = random.randrange(0,101)
-    if mutationindex>93:
+    for i in range(len(monster1)):
+        mutationindex = random.randrange(1,101)
+        if mutationindex>93:
+            babo.append(random.randrange(1,10))
+        else:
+            chance = random.randrange(1,101)
+            if chance>50:
+                babo.append(monster2[i])
+            else:
+                babo.append(monster1[i])
+    return babo
 
 
 
