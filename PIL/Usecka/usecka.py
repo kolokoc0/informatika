0c1cbc42b11579977830fa04f47c7119a842115c
kolokoc0 <kristiansvrlo@gmail.com>
2023-02-26 12:07:49 +0100

    teraz to fakt je ono

--- PIL/Usecka/usecka.py
+++ PIL/Usecka/usecka.py
@@ -10,24 +10,36 @@ b=list(map(lambda x: int(x), b))
 if a[0]>b[0] and a[1] > b[1] or a[0]>b[0] and a[1]<b[1]:
     a,b = b,a
 
-k=(b[1]-a[1])/(b[0]-a[0])
-q=a[1]-k*a[0]
-temp = int(k*a[0]+q)
-for x in range(a[0],b[0]+1):
-    y=int(k*x+q)
-    pixels[x,y]=(255,0,0)
-    print(x,y)
-    vektor = (b[0]-a[0],b[1]-a[1])
-    vektor = (vektor[1],vektor[0]*(-1))
-    if vektor[0]<0 and vektor[1]<0:
-        vektor = (vektor[0]*(-1),vektor[1]*(-1))
-    c = float((-1)*(vektor[0]*a[0] + vektor[1]*a[1]))
-    if (abs(y -temp)) >1:
-        for i in range(1,abs(y-temp)):
-            if y<temp:
-                i = i*(-1)
-            new_x = round((vektor[1]*(temp+i)*(-1)+c*(-1))/(vektor[0]))
-            print(new_x,temp+i)
-            pixels[new_x,temp+i] = (255,0,0) 
-    temp = y
+if b[0]-a[0] != 0:
+    k=(b[1]-a[1])/(b[0]-a[0])
+    q=a[1]-k*a[0]
+    temp = int(k*a[0]+q)
+    for x in range(a[0],b[0]+1):
+        y=int(k*x+q)
+        pixels[x,y]=(255,0,0)
+        print(x,y)
+        vektor = (b[0]-a[0],b[1]-a[1])
+        vektor = (vektor[1],vektor[0]*(-1))
+        if vektor[0]<0 and vektor[1]<0:
+            vektor = (vektor[0]*(-1),vektor[1]*(-1))
+        c = float((-1)*(vektor[0]*a[0] + vektor[1]*a[1]))
+        if (abs(y -temp)) >1:
+            for i in range(1,abs(y-temp)):
+                if y<temp:
+                    i = i*(-1)
+                new_x = round((vektor[1]*(temp+i)*(-1)+c*(-1))/(vektor[0]))
+                print(new_x,temp+i)
+                pixels[new_x,temp+i] = (255,0,0) 
+        temp = y
+else:
+    if a[1]>b[1]:
+        a[1],b[1] = b[1],a[1]
+    for y in range(a[1],b[1]+1):
+        pixels[a[0],y] = (255,0,0)
+
 img.show()
+
+
+
+
+
